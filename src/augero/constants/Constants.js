fcca8ec297a29cf152bac298e171284ff23dba64
Ciobanu Vlad <ciobanu.vlad@cerner.com>
2019-02-25 13:45:03 +0200

    change Constants file

--- src/augero/constants/Constants.js
+++ src/augero/constants/Constants.js
@@ -5,6 +5,7 @@ sap.ui.define([], function () {
     // const HAPI_FHIR_PAGINATION_SERVER = "https://hapi-fhir-page.cfapps.eu10.hana.ondemand.com";
     this.HAPI_FHIR_SERVER = "https://hapi-fhir.cfapps.eu10.hana.ondemand.com";
     this.HAPI_FHIR_PAGINATION_SERVER = "https://hapi-fhir-page.cfapps.eu10.hana.ondemand.com";
+	this.HAPI_TEST = "https://hapi-fhir-page.cfapps.eu10.hana.ondemand.com";
     // this.
     return this;
 });
