1fc0ff7a1eda6e6cb0a163990d57b1f1596fb212
Ciobanu Vlad <ciobanu.vlad@cerner.com>
2019-02-25 11:53:22 +0200

    add constannts file

---
+++ src/ui5lab/geometry/Constants.js
@@ -0,0 +1,10 @@
+sap.ui.define([], function () {
+    "use strict";
+    debugger;
+    // const HAPI_FHIR_SERVER = "https://hapi-fhir.cfapps.eu10.hana.ondemand.com";
+    // const HAPI_FHIR_PAGINATION_SERVER = "https://hapi-fhir-page.cfapps.eu10.hana.ondemand.com";
+    this.HAPI_FHIR_SERVER = "https://hapi-fhir.cfapps.eu10.hana.ondemand.com";
+    this.HAPI_FHIR_PAGINATION_SERVER = "https://hapi-fhir-page.cfapps.eu10.hana.ondemand.com";
+    // this.
+    return this;
+});
